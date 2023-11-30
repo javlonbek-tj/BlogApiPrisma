9c6e9848b23f2f460791776f1d9752107d7796a9
Javlonbek Turdimatov <102795285+javlonbek-tj@users.noreply.github.com>
2023-11-30 17:53:52 +0500

    Delete dist/src directory

--- dist/src/utils/db.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const client_1 = require("@prisma/client");
-let db;
-if (!global.__db) {
-    global.__db = new client_1.PrismaClient();
-}
-db = global.__db;
-exports.default = db;
